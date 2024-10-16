a04f73d150299c787152c521d5dbe583545429fc
Tên của bạn <email@example.com>
2024-10-16 23:50:36 +0700

    Updat

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FileUploadController;
 
-Route::get('/upload', function () {
+Route::get('upload', function () {
     return view('upload');
 });
 
-Route::post('/upload', [FileUploadController::class, 'upload'])->name('upload');
+Route::post('upload', [FileUploadController::class, 'upload'])->name('upload');
